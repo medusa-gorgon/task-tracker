ea3a6ae242dc784f4140d698bae86eaf3284e642
mg-bizarre <mg.bizarre@gmail.com>
2022-03-14 12:03:49 +0100

    update README.md

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -20,16 +20,33 @@ const AddTask = ({ addTask }) => {
     <div className='add__task'>
       <form action='' className='add__form' onSubmit={onSubmit}>
         <div className='form__control'>
-          <label htmlFor=''>New task</label>
-          <input type='text' placeholder='enter a task' value={text} onChange={(e) => setText(e.target.value)} />
+          <label htmlFor='newTask'>New task</label>
+          <input
+            id='newTask'
+            type='text'
+            placeholder='enter a task'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
         </div>
         <div className='form__control'>
-          <label htmlFor=''>Date</label>
-          <input type='text' placeholder='enter date and time' value={date} onChange={(e) => setDate(e.target.value)} />
+          <label htmlFor='newTaskDate'>Date</label>
+          <input
+            id='newTaskDate'
+            type='text'
+            placeholder='enter date and time'
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
         </div>
         <div className='form__control form__check'>
-          <label htmlFor=''>Set a reminder</label>
-          <input type='checkbox' value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
+          <label htmlFor='newTaskReminder'>Set a reminder</label>
+          <input
+            id='newTaskReminder'
+            type='checkbox'
+            value={reminder}
+            onChange={(e) => setReminder(e.currentTarget.checked)}
+          />
         </div>
         <input className='btn btn__form' type='submit' value='Save task' />
       </form>
